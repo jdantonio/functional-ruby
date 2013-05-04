964b34b3dfeed96cf865cbc42d6083ca06d62a3b
Jerry D'Antonio <stumpjumper@gmail.com>
2013-05-04 08:48:17 -0400

    Now dynamically defines function and dispatches to pattern matcher

--- lib/pattern_matching.rb
+++ lib/pattern_matching.rb
@@ -4,15 +4,18 @@ module PatternMatching
 
   def self.included(base)
 
+    def __pattern_match__(func, *args, &block)
+      #puts "Calling function ##{func} with args #{args}"
+    end
+
     class << base
 
       def defn(func, *args, &block)
         #puts "Defining function ##{func} with args #{args}"
-        #if block_given?
-          #self.send(:define_method, func, &block)
-        #else
-          #self.send(:define_method, func){}
-        #end
+        block = Proc.new{} unless block_given?
+        self.send(:define_method, func) do |*args, &block|
+          __pattern_match__(func, *args, block)
+        end
       end
 
     end
