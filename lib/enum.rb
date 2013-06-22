5bea22d1fa3e491b963aa119c87eafa922a2fdae
Jerry D'Antonio <stumpjumper@gmail.com>
2013-06-21 23:54:54 -0400

    Brainstormed future functionality

--- lib/enum.rb
+++
@@ -1,3 +0,0 @@
-# http://docs.oracle.com/javase/tutorial/java/javaOO/enum.html
-# http://www.lesismore.co.za/rubyenums.html
-# http://gistflow.com/posts/682-ruby-enums-approaches
---
+++ lib/pattern_matching/concurrency.rb
@@ -0,0 +1,27 @@
+# http://docs.oracle.com/javase/tutorial/java/javaOO/enum.html
+# http://www.lesismore.co.za/rubyenums.html
+# http://gistflow.com/posts/682-ruby-enums-approaches
+
+# http://richhickey.github.io/clojure/clojure.core-api.html
+# * agent
+# * add-watch
+# * apply
+# * assert
+# * await
+# * future
+# * memoize
+# * promise
+# * send
+# * slurp
+
+# Other stuff
+# * pure - creates an object, freezes it, and removes the unfreeze method
+# * retry - retries something x times if it fails
+# * promise = ala JavaScript http://blog.parse.com/2013/01/29/whats-so-great-about-javascript-promises/
+# * ada range type - http://en.wikibooks.org/wiki/Ada_Programming/Types/range
+# * slurpee - slurp + erb parsing
+# * spawn/send/receive - http://www.erlang.org/doc/reference_manual/processes.html
+
+# Using Ruby's queue for sending messages between threads
+# * http://www.subelsky.com/2010/02/using-rubys-queue-class-to-manage-inter.html
+# * http://www.ruby-doc.org/stdlib-1.9.3/libdoc/thread/rdoc/Queue.html#method-i-pop
