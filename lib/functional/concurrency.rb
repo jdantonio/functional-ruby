552fcd3634ee2c93d566d6a25cb05cecb7a26257
Jerry D'Antonio <jdantonio@virtualhold.com>
2013-06-24 16:00:00 -0400

    Added more links to promise documentation

--- lib/functional/concurrency.rb
+++ lib/functional/concurrency.rb
@@ -155,7 +155,9 @@ end
 # Other stuff
 # * pure - creates an object, freezes it, and removes the unfreeze method
 # * retry - retries something x times if it fails
-# * promise = ala JavaScript http://blog.parse.com/2013/01/29/whats-so-great-about-javascript-promises/
+# * promise - ala JavaScript http://blog.parse.com/2013/01/29/whats-so-great-about-javascript-promises/
+#           - http://domenic.me/2012/10/14/youre-missing-the-point-of-promises/
+#           - http://www.slideshare.net/domenicdenicola/callbacks-promises-and-coroutines-oh-my-the-evolution-of-asynchronicity-in-javascript
 # * ada range type - http://en.wikibooks.org/wiki/Ada_Programming/Types/range
 # * slurpee - slurp + erb parsing
 # * spawn/send/receive - http://www.erlang.org/doc/reference_manual/processes.html
