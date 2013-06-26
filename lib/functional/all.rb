36b7719cdc2609d5ef6501ce0a40a91f11fe48ec
Jerry D'Antonio <stumpjumper@gmail.com>
2013-06-25 23:38:29 -0400

    Renamed 'core' to 'utilities'

--- lib/functional/all.rb
+++ lib/functional/all.rb
@@ -1,6 +1,6 @@
 require 'functional/behavior'
 require 'functional/concurrency'
-require 'functional/core'
 require 'functional/pattern_matching'
 require 'functional/promise'
+require 'functional/utilities'
 require 'functional/version'
--- spec/functional/utilities_spec.rb
+++ spec/functional/utilities_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe 'core' do
+describe 'utilities' do
 
   context '#repl?' do
     pending
