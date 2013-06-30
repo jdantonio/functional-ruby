7c38b9eda71b71ad90b163bec8b91afb9f074009
Jerry D'Antonio <stumpjumper@gmail.com>
2013-06-29 23:56:16 -0400

    Updated utilities

--- TODO.md
+++ TODO.md
@@ -14,31 +14,9 @@ parameter. The return value of the block will become the new value of the agent.
 two error handling modes: fail and continue. A good example of an agent is a shared incrementing
 counter.
 
-Erlang process/Go goroutine: Erlang and Go have very lightweight concurrency mechanisms
-(processes and goroutines respectively). A process can be launched at any time and given
-a function. It will run the function concurrently then die. The return value will not
-go anywhere so side-effects are necessary. Ruby threads are not nearly as lightweight
-as Erlang/Go processes, but used sparingly the simplicity of the model is appealing.
-
 Erlang/Go send-receive/channel: Built on their lightweight processes mechanisms, both
 Erlang and Go support message passing send/receive loops between processes. The heavyweight
 threads in Ruby make long-lived send/receive loops compelling.
-
-Clojure future: At this point, the differences between concurrency tools becomes more subtle.
-Clojure futures are very similar to Clojure agents and Go goroutines. A future is simply a
-concurrent process that will contain a value once complete. While the future is working
-the value cannot be retrieved (and supports blocking deref calls). Once the future is
-complete it will forevermore have the finished value. Deref calls at that point do not block
-
-JavaScript promises: A chained series of behavioral routines and associated error handlers.
-Excellent for handling a series of actions on a different thread. Promises interact with
-the outside world through side-effects.
-
-* http://wiki.commonjs.org/wiki/Promises/A
-* http://promises-aplus.github.io/promises-spec/
-* http://blog.parse.com/2013/01/29/whats-so-great-about-javascript-promises/
-* http://domenic.me/2012/10/14/youre-missing-the-point-of-promises/
-* http://www.slideshare.net/domenicdenicola/callbacks-promises-and-coroutines-oh-my-the-evolution-of-asynchronicity-in-javascript
 
 ## Enums
 
@@ -63,28 +41,17 @@ http://richhickey.github.io/clojure/clojure.core-api.html
 * [set-validator](http://clojuredocs.org/clojure_core/clojure.core/set-validator!)
 * [deref](http://clojuredocs.org/clojure_core/clojure.core/deref)
 
-### Futures
-
-* [future](http://clojuredocs.org/clojure_core/clojure.core/future)
-* [realized?](http://clojuredocs.org/clojure_core/clojure.core/realized_q)
-* [future?](http://clojuredocs.org/clojure_core/clojure.core/future_q)
-* [future-cancel](http://clojuredocs.org/clojure_core/clojure.core/future-cancel)
-* [deref](http://clojuredocs.org/clojure_core/clojure.core/deref)
-* [shutdown-agents](http://clojuredocs.org/clojure_core/clojure.core/shutdown-agents)
-
 ### Core
 
 * [apply](http://clojuredocs.org/clojure_core/clojure.core/apply)
 * [delay](http://clojuredocs.org/clojure_core/clojure.core/delay)
 * [await](http://clojuredocs.org/clojure_core/clojure.core/await)
 * [memoize](http://clojuredocs.org/clojure_core/clojure.core/memoize)
-* [slurp](http://clojuredocs.org/clojure_core/clojure.core/slurp)
 
 ## Other stuff
 
 * retry - retries something x times if it fails
 * ada range type - http://en.wikibooks.org/wiki/Ada_Programming/Types/range
-* slurpee - slurp + erb parsing
 
 ### Using Ruby's queue for sending messages between threads
 
--- lib/functional/utilities.rb
+++ lib/functional/utilities.rb
@@ -7,12 +7,28 @@ NaN = 0/0.0 unless defined?(NaN)
 
 module Kernel
 
-  # Is the current process a REPL? 
-  # @private
-  def repl? # :nodoc:
-    return ($0 == 'irb' || $0 == 'pry' || $0 == 'script/rails' || !!($0 =~ /bin\/bundle$/))
+  # Compute the difference (delta) between two values.
+  #
+  # When a block is given the block will be applied to both arguments.
+  # Using a block in this way allows computation against a specific field
+  # in a data set of hashes or objects.
+  #
+  # @yield iterates over each element in the data set
+  # @yieldparam item each element in the data set
+  #
+  # @param [Object] v1 the first value
+  # @param [Object] v2 the second value
+  #
+  # @return [Float] positive value representing the difference
+  #   between the two parameters
+  def delta(v1, v2)
+    if block_given?
+      v1 = yield(v1)
+      v2 = yield(v2)
+    end
+    return (v1 - v2).abs
   end
-  module_function :repl?
+  module_function :delta
 
   # Sandbox the given operation at a high $SAFE level.
   #
@@ -20,29 +36,17 @@ module Kernel
   # @param block [Proc] the block to isolate
   #
   # @return [Object] the result of the block operation
-  def safe(*args, &block)
+  def safe(*args)
     raise ArgumentError.new('no block given') unless block_given?
     result = nil
     t = Thread.new do
       $SAFE = 3
-      result = self.instance_exec(*args, &block)
+      result = yield(*args)
     end
     t.join
     return result
   end
   module_function :safe
-
-  # @private
-  # @see http://rhaseventh.blogspot.com/2008/07/ruby-and-rails-how-to-get-pp-pretty.html
-  def pp_s(*objs) # :nodoc:
-    s = StringIO.new
-    objs.each {|obj|
-      PP.pp(obj, s)
-    }
-    s.rewind
-    s.read
-  end
-  module_function :pp_s
 
   # Open a file, read it, close the file, and return its contents.
   #
@@ -68,27 +72,38 @@ module Kernel
   end
   module_function :slurpee
 
-  # Compute the difference (delta) between two values.
-  # 
-  # When a block is given the block will be applied to both arguments.
-  # Using a block in this way allows computation against a specific field
-  # in a data set of hashes or objects.
-  #
-  # @yield iterates over each element in the data set
-  # @yieldparam item each element in the data set
-  #
-  # @param [Object] v1 the first value
-  # @param [Object] v2 the second value
-  #
-  # @return [Float] positive value representing the difference
-  #   between the two parameters
-  def delta(v1, v2)
-    if block_given?
-      v1 = yield(v1)
-      v2 = yield(v2)
-    end
-    return (v1 - v2).abs
-  end
-  module_function :delta
+  #############################################################################
 
+  # @private
+  def repl? # :nodoc:
+    return ($0 == 'irb' || $0 == 'pry' || $0 == 'script/rails' || !!($0 =~ /bin\/bundle$/))
+  end
+  module_function :repl?
+
+  # @private
+  def timer(*args) # :nodoc:
+    t1 = Time.now
+    result = yield(*args)
+    t2 = Time.now
+    return (t2 - t1)
+  end
+  module_function :timer
+
+  # @private
+  def strftimer(seconds) # :nodoc:
+    Time.at(seconds).gmtime.strftime('%R:%S.%L')
+  end
+  module_function :strftimer
+
+  # @private
+  # @see http://rhaseventh.blogspot.com/2008/07/ruby-and-rails-how-to-get-pp-pretty.html
+  def pp_s(*objs) # :nodoc:
+    s = StringIO.new
+    objs.each {|obj|
+      PP.pp(obj, s)
+    }
+    s.rewind
+    s.read
+  end
+  module_function :pp_s
 end
