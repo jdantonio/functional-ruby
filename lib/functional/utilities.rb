5f940f8d390bb10fc74d0e15a87b6352894597be
Jerry D'Antonio <stumpjumper@gmail.com>
2013-07-26 22:11:05 -0400

    Added the Kernel#atomic function

--- lib/functional/utilities.rb
+++ lib/functional/utilities.rb
@@ -7,6 +7,26 @@ NaN = 0/0.0 unless defined?(NaN)
 
 module Kernel
 
+  # Perform the given block as though it were an atomic operation. This means
+  # that the Ruby scheduler cannot premept the block and context switch to
+  # another thread. Basically a light wrapper around Ruby's Fiber class.
+  #
+  # @note Be very careful about what operations you perform within an atomic
+  # block. Blocking operations such as I/O should *never* occur within an
+  # atomic block. In those cases the entire Ruby VM will lock until the
+  # blocking operation is complete. This would be bad.
+  #
+  # @yield calls the block
+  # @yieldparam args an arbitrary set of block arguments
+  #
+  # @param [Array] zero more more optional arguments to pass to the block
+  def atomic(*args)
+    return Fiber.new {
+      yield(*args)
+    }.resume
+  end
+  module_function :atomic
+
   # Compute the difference (delta) between two values.
   #
   # When a block is given the block will be applied to both arguments.
