7e4c2c2a53ebcf9df54a815e9402fbe211ef5eaa
Jerry D'Antonio <stumpjumper@gmail.com>
2014-06-02 23:29:15 -0400

    Removed all algorithms (collections, sort, search, etc.) classes.

--- lib/functional.rb
+++ lib/functional.rb
@@ -1,13 +1,6 @@
 require 'functional/behavior'
 require 'functional/behaviour'
-require 'functional/catalog'
-require 'functional/collection'
-require 'functional/inflect'
 require 'functional/pattern_matching'
-require 'functional/platform'
-require 'functional/search'
-require 'functional/sort'
-require 'functional/utilities'
 require 'functional/version'
 
 Infinity = 1/0.0 unless defined?(Infinity)
@@ -17,10 +10,4 @@ $ENABLE_BEHAVIOR_CHECK_ON_CONSTRUCTION ||= true
 
 module Functional
 
-  class << self
-    include Collection
-    include Inflect
-    include Search
-    include Sort
-  end
 end
