7cb5d8935e05e1424a1643124b34005c144bc9f2
Jerry D'Antonio <stumpjumper@gmail.com>
2014-07-15 23:08:26 -0400

    Added several methods to Either.

--- lib/functional/either.rb
+++ lib/functional/either.rb
@@ -7,11 +7,11 @@ module Functional
     # class methods
 
     def self.left(v)
-      new(v, nil, true)
+      new(v, nil, true).freeze
     end
 
     def self.right(v)
-      new(nil, v, false)
+      new(nil, v, false).freeze
     end
 
     class << self
@@ -38,6 +38,25 @@ module Functional
 
     def swap
       self.class.send(:new, @right, @left, ! @is_left)
+    end
+
+    def either(lproc, rproc)
+      if left?
+        lproc.call(left)
+      else
+        rproc.call(right)
+      end
+    end
+
+    def self.iff(*args)
+      raise ArgumentError.new("wrong number of arguments (#{args.length} for 2..3)") if args.length < 2 || args.length > 3
+      raise ArgumentError.new('requires either a boolean expression or a block, not both') if args.length == 3 && block_given?
+      boolean = block_given? ? yield : !! args.last
+      boolean ? left(args[0]) : right(args[1])
+    end
+
+    def reduce
+      left? ? left : right
     end
 
     private
--- spec/functional/either_spec.rb
+++ spec/functional/either_spec.rb
@@ -26,6 +26,11 @@ module Functional
         expect(Either.right(value).right).to eq value
       end
 
+      it 'freezes the new object' do
+        expect(Either.left(:foo)).to be_frozen
+        expect(Either.right(:foo)).to be_frozen
+      end
+
       it 'aliases #left to #reason' do
         expect(Either.reason(value).left).to eq value
       end
@@ -104,6 +109,111 @@ module Functional
         expect(swapped).to be_left
         expect(swapped.right).to be_nil
         expect(swapped.left).to eq :foo
+      end
+    end
+
+    context '#either' do
+
+      it 'passes the left value to the left proc when left' do
+        expected = nil
+        subject = Either.left(100)
+        subject.either(
+          ->(left) { expected = left },
+          ->(right) { expected = -1 }
+        )
+        expect(expected).to eq 100
+      end
+
+      it 'returns the value of the left proc when left' do
+        subject = Either.left(100)
+        expect(
+          subject.either(
+            ->(left) { left * 2 },
+            ->(right) { nil }
+          )
+        ).to eq 200
+      end
+
+      it 'passes the right value to the right proc when right' do
+        expected = nil
+        subject = Either.right(100)
+        subject.either(
+          ->(right) { expected = -1 },
+          ->(right) { expected = right }
+        )
+        expect(expected).to eq 100
+      end
+
+      it 'returns the value of the right proc when right' do
+        subject = Either.right(100)
+        expect(
+          subject.either(
+            ->(right) { nil },
+            ->(right) { right * 2 }
+          )
+        ).to eq 200
+      end
+    end
+
+    context '#iif' do
+
+      it 'returns a lefty with the given left value when the boolean is true' do
+        subject = Either.iff(:foo, :bar, true)
+        expect(subject).to be_left
+        expect(subject.left).to eq :foo
+      end
+
+      it 'returns a righty with the given right value when the boolean is false' do
+        subject = Either.iff(:foo, :bar, false)
+        expect(subject).to be_right
+        expect(subject.right).to eq :bar
+      end
+
+      it 'returns a lefty with the given left value when the block is truthy' do
+        subject = Either.iff(:foo, :bar){ :baz }
+        expect(subject).to be_left
+        expect(subject.left).to eq :foo
+      end
+
+      it 'returns a righty with the given right value when the block is false' do
+        subject = Either.iff(:foo, :bar){ false }
+        expect(subject).to be_right
+        expect(subject.right).to eq :bar
+      end
+
+      it 'returns a righty with the given right value when the block is nil' do
+        subject = Either.iff(:foo, :bar){ nil }
+        expect(subject).to be_right
+        expect(subject.right).to eq :bar
+      end
+
+      it 'raises an exception when given less than two arguments' do
+        expect {
+          subject = Either.iff(nil)
+        }.to raise_error(ArgumentError)
+      end
+
+      it 'raises an exception when given more than three arguments' do
+        expect {
+          subject = Either.iff(1, 2, 3, 4)
+        }.to raise_error(ArgumentError)
+      end
+
+      it 'raises an exception when both a boolean and a block are given' do
+        expect {
+          subject = Either.iff(:foo, :bar, true){ nil }
+        }.to raise_error(ArgumentError)
+      end
+    end
+
+    context '#reduce' do
+
+      it 'returns the left value of a lefty' do
+        expect(Either.left(:foo).reduce).to eq (:foo)
+      end
+
+      it 'returns the right value of a righty' do
+        expect(Either.right(:bar).reduce).to eq (:bar)
       end
     end
   end
