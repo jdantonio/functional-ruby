90227a9bec68a1db6ace7d32f3d6830bd9e25bfb
Jerry D'Antonio <stumpjumper@gmail.com>
2014-07-17 16:21:23 -0400

    Initial implementation of BehavesAs? with tests

--- lib/functional/behavior_check.rb
+++ lib/functional/behavior_check.rb
@@ -21,12 +21,12 @@ module Functional
       def initialize(info)
         @info = info
       end
-      def method(name, arity = Functional::BehaviorCheck::ANY_ARITY)
-        arity = arity.to_i unless arity == Functional::BehaviorCheck::ANY_ARITY
+      def method(name, arity = nil)
+        arity = arity.to_i unless arity.nil?
         @info[:methods][name.to_sym] = arity
       end
-      def class_method(name, arity = Functional::BehaviorCheck::ANY_ARITY)
-        arity = arity.to_i unless arity == Functional::BehaviorCheck::ANY_ARITY
+      def class_method(name, arity = nil)
+        arity = arity.to_i unless arity.nil?
         @info[:class_methods][name.to_sym] = arity
       end
     end
@@ -40,42 +40,58 @@ module Functional
   end
   module_function :Behavior
 
-  def BehaveAs?(behavior, *behaviors)
-  end
-  alias_method :BehavesAs?, :BehaveAs?
-  module_function :BehaveAs?
-
-  def BehaveAs!(behavior, *behaviors)
-  end
-  alias_method :BehavesAs!, :BehaveAs!
-  module_function :BehaveAs!
-
-  def BehaviorDefined?(*behaviors)
-  end
-  module_function :BehaviorDefined?
-
-  def BehaviorDefined!(*behaviors)
-  end
-  module_function :BehaviorDefined!
-
   module BehaviorCheck
 
     @@info = {}
 
-    ANY_ARITY = BasicObject.new
-
     def BehaveAs?(target, *behaviors)
+      results = behaviors.drop_while do |behavior|
+        behave_as?(target, behavior.to_sym)
+      end
+      results.empty?
     end
     alias_method :BehavesAs?, :BehaveAs?
+    module_function :BehaveAs?
+    module_function :BehavesAs?
 
     def BehaveAs!(target, *behaviors)
     end
     alias_method :BehavesAs!, :BehaveAs!
+    module_function :BehaveAs!
+    module_function :BehavesAs!
 
     def BehaviorDefined?(*behaviors)
     end
+    module_function :BehaviorDefined?
 
     def BehaviorDefined!(*behaviors)
+    end
+    module_function :BehaviorDefined!
+
+    private
+
+    def self.check_arity?(target, method, arity)
+      expected = target.method(method).arity
+      arity.nil? || arity == expected
+    rescue
+      return false
+    end
+
+    def self.behave_as?(target, behavior)
+      clazz = target.is_a?(Module) ? target : target.class
+      info = @@info[behavior]
+      return false if info.nil?
+
+      results = info[:methods].drop_while do |method, arity|
+        check_arity?(target, method, arity)
+      end
+      return false unless results.empty?
+
+      results = info[:class_methods].drop_while do |method, arity|
+        check_arity?(clazz, method, arity)
+      end
+
+      results.empty?
     end
   end
 end
--- spec/functional/behavior_check_spec.rb
+++ spec/functional/behavior_check_spec.rb
@@ -1,83 +1,313 @@
 require 'spec_helper'
 
-Functional::BehaviorInfo :gen_foo do
-  method :foo # any arity
-  method :bar, 2
-  method :baz, -2
-  class_method :foo # any arity
-  class_method :bar, 2
-  class_method :baz, -2
-  constant :foo
-  constant :baz_bar
-end
+#Functional::BehaviorInfo :gen_foo do
+  #method :foo # any arity
+  #method :bar, 2
+  #method :baz, -2
+  #class_method :foo # any arity
+  #class_method :bar, 2
+  #class_method :baz, -2
+#end
 
-behavior_info = Functional::BehaviorInfo(:gen_foo)
+#behavior_info = Functional::BehaviorInfo(:gen_foo)
 
-class MyClass
-  include Functional::BehaviorCheck
+#class MyClass
+  #include Functional::BehaviorCheck
 
-  def do_stuff(first, second)
-    BehaveAs? first, :foobar
-    BehaveAs! second, :gen_foo
-    BehaviorDefined? :foobar
-    BehaviorDefined! :gen_foo
-  end
-end
+  #def do_stuff(first, second)
+    #BehaveAs? first, :foobar
+    #BehaveAs! second, :gen_foo
+    #BehaviorDefined? :foobar
+    #BehaviorDefined! :gen_foo
+  #end
+#end
 
-class ThisClass
-  Functional::Behavior :gen_foo
-end
+#class ThisClass
+  #Functional::Behavior :gen_foo
+#end
 
-class ThisModule
-  Functional::Behavior :gen_foo
-end
+#class ThisModule
+  #Functional::Behavior :gen_foo
+#end
 
-Functional::BehaveAs? ThisClass, :foobar
-Functional::BehaveAs! ThisModule, :gen_foo
+#Functional::BehaviorCheck::BehaveAs? ThisClass, :foobar
+#Functional::BehaviorCheck::BehaveAs! ThisModule, :gen_foo
 
-Functional::BehaviorDefined? :foobar
-Functional::BehaviorDefined! :gen_foo
+#Functional::BehaviorCheck::BehaviorDefined? :foobar
+#Functional::BehaviorCheck::BehaviorDefined! :gen_foo
 
-module Functional
+describe 'behavior specification' do
 
-  describe 'BehaviorInfo' do
-    pending
-  end
-
-  describe 'Behavior' do
-    pending
+  before(:each) do
+    @behavior_info = Functional::BehaviorCheck.class_variable_get(:@@info)
+    Functional::BehaviorCheck.class_variable_set(:@@info, {})
   end
 
-  describe 'BehaveAs?' do
-    pending
+  after(:each) do
+    Functional::BehaviorCheck.class_variable_set(:@@info, @behavior_info)
   end
 
-  describe 'BehavesAs!' do
-    pending
-  end
+  context 'BehaviorInfo method' do
 
-  describe 'BehaviorDefined?' do
-    pending
-  end
+    context 'without a block' do
 
-  describe 'BehaviorDefined!' do
-    pending
-  end
+      it 'returns the specified behavior when defined' do
+        Functional::BehaviorInfo(:foo){ nil }
+        expect(Functional::BehaviorInfo(:foo)).to_not be_nil
+      end
 
-  describe BehaviorCheck do
-
-    before(:each) do
-      @behavior_info = BehaviorCheck.class_variable_get(:@@info)
-      BehaviorCheck.class_variable_set(:@@info, {})
+      it 'returns nil when not defined' do
+        expect(Functional::BehaviorInfo(:foo)).to be_nil
+      end
     end
 
-    after(:each) do
-      BehaviorCheck.class_variable_set(:@@info, @behavior_info)
-    end
+    context 'with a block' do
 
+      it 'raises an exception if the behavior has already been specified' do
+        Functional::BehaviorInfo(:foo){ nil }
+
+        expect {
+          Functional::BehaviorInfo(:foo){ nil }
+        }.to raise_error(Functional::BehaviorError)
+      end
+
+      it 'specifies an instance method with any arity' do
+        Functional::BehaviorInfo :foo do
+          method :foo
+        end
+
+        info = Functional::BehaviorInfo(:foo)
+        expect(info[:methods][:foo]).to be_nil
+      end
+
+      it 'specifies an instance method with a given arity' do
+        Functional::BehaviorInfo :foo do
+          method :foo, 2
+        end
+
+        info = Functional::BehaviorInfo(:foo)
+        expect(info[:methods][:foo]).to eq 2
+      end
+
+      it 'specifies a class method with any arity' do
+        Functional::BehaviorInfo :foo do
+          class_method :foo
+        end
+
+        info = Functional::BehaviorInfo(:foo)
+        expect(info[:class_methods][:foo]).to be_nil
+      end
+
+      it 'specifies a class method with a given arity' do
+        Functional::BehaviorInfo :foo do
+          class_method :foo, 2
+        end
+
+        info = Functional::BehaviorInfo(:foo)
+        expect(info[:class_methods][:foo]).to eq 2
+      end
+    end
+  end
+
+  describe Functional::BehaviorCheck do
 
     context 'BehaveAs?' do
-      pending
+
+      it 'validates methods with no parameters' do
+        Functional::BehaviorInfo(:foo) do
+          method(:bar, 0)
+          class_method(:baz, 0)
+        end
+
+        clazz = Class.new do
+          def bar(); nil; end
+          def self.baz(); nil; end
+        end
+
+        expect(Functional::BehaviorCheck::BehaveAs?(clazz.new, :foo)).to be true
+      end
+
+      it 'validates methods with a fixed number of parameters' do
+        Functional::BehaviorInfo(:foo) do
+          method(:bar, 3)
+          class_method(:baz, 3)
+        end
+
+        clazz = Class.new do
+          def bar(a,b,c); nil; end
+          def self.baz(a,b,c); nil; end
+        end
+
+        expect(Functional::BehaviorCheck::BehaveAs?(clazz.new, :foo)).to be true
+      end
+
+      it 'validates methods with optional parameters' do
+        Functional::BehaviorInfo(:foo) do
+          method(:bar, -2)
+          class_method(:baz, -2)
+        end
+
+        clazz = Class.new do
+          def bar(a, b=1); nil; end
+          def self.baz(a, b=1, c=2); nil; end
+        end
+
+        expect(Functional::BehaviorCheck::BehaveAs?(clazz.new, :foo)).to be true
+      end
+
+      if RUBY_VERSION >= '2.0'
+        it 'validates methods with keyword parameters' do
+          Functional::BehaviorInfo(:foo) do
+            method(:bar, -2)
+            class_method(:baz, -3)
+          end
+
+          clazz = Class.new do
+            def bar(a, foo: 'foo', baz: 'baz'); nil; end
+            def self.baz(a, b, foo: 'foo', baz: 'baz'); nil; end
+          end
+
+          expect(Functional::BehaviorCheck::BehaveAs?(clazz.new, :foo)).to be true
+        end
+      end
+
+      it 'validates methods with variable length argument lists' do
+        Functional::BehaviorInfo(:foo) do
+          method(:bar, -2)
+          class_method(:baz, -3)
+        end
+
+        clazz = Class.new do
+          def bar(a, *args); nil; end
+          def self.baz(a, b, *args); nil; end
+        end
+
+        expect(Functional::BehaviorCheck::BehaveAs?(clazz.new, :foo)).to be true
+      end
+
+      it 'validates methods with arity -1' do
+        Functional::BehaviorInfo(:foo) do
+          method(:bar, -1)
+          class_method(:baz, -1)
+        end
+
+        clazz = Class.new do
+          def bar(*args); nil; end
+          def self.baz(*args); nil; end
+        end
+
+        expect(Functional::BehaviorCheck::BehaveAs?(clazz.new, :foo)).to be true
+      end
+
+      it 'validates classes' do
+        Functional::BehaviorInfo(:foo) do
+          class_method(:baz, -3)
+        end
+
+        clazz = Class.new do
+          def self.baz(a, b, *args); nil; end
+        end
+
+        expect(Functional::BehaviorCheck::BehaveAs?(clazz, :foo)).to be true
+      end
+
+      it 'validates modules' do
+        Functional::BehaviorInfo(:foo) do
+          class_method(:baz, -3)
+        end
+
+        clazz = Module.new do
+          def bar(a, *args); nil; end
+          def self.baz(a, b, *args); nil; end
+        end
+
+        expect(Functional::BehaviorCheck::BehaveAs?(clazz, :foo)).to be true
+      end
+
+      it 'always accepts methods when arity not given' do
+        Functional::BehaviorInfo(:foo) do
+          method(:foo)
+          method(:bar)
+          method(:baz)
+          class_method(:foo)
+          class_method(:bar)
+          class_method(:baz)
+        end
+
+        clazz = Class.new do
+          def foo(); nil; end
+          def bar(a, b, c); nil; end
+          def baz(a, b, *args); nil; end
+          def self.foo(); nil; end
+          def self.bar(a, b, c); nil; end
+          def self.baz(a, b, *args); nil; end
+        end
+
+        expect(Functional::BehaviorCheck::BehaveAs?(clazz.new, :foo)).to be true
+      end
+
+      it 'accepts and checks multiple behaviors' do
+        Functional::BehaviorInfo(:foo){ method(:foo) }
+        Functional::BehaviorInfo(:bar){ method(:foo) }
+        Functional::BehaviorInfo(:baz){ method(:foo) }
+
+        clazz = Class.new do
+          def foo(); nil; end
+        end
+
+        expect(
+          Functional::BehaviorCheck::BehaveAs?(clazz.new, :foo, :bar, :baz)
+        ).to be true
+      end
+
+      it 'returns false if one or more instance methods do not match' do
+        Functional::BehaviorInfo(:foo) do
+          method(:bar, 0)
+        end
+
+        clazz = Class.new do
+          def bar(a, b, *args); nil; end
+        end
+
+        expect(Functional::BehaviorCheck::BehaveAs?(clazz.new, :foo)).to be false
+      end
+
+      it 'returns false if one or more class methods do not match' do
+        Functional::BehaviorInfo(:foo) do
+          class_method(:bar, 0)
+        end
+
+        clazz = Class.new do
+          def bar(a, b, *args); nil; end
+        end
+
+        expect(Functional::BehaviorCheck::BehaveAs?(clazz.new, :foo)).to be false
+      end
+
+      it 'returns false if one or more behaviors has not been defined' do
+        Functional::BehaviorInfo(:foo) do
+          method(:bar, 0)
+          class_method(:bar, 0)
+        end
+
+        expect(
+          Functional::BehaviorCheck::BehaveAs?('object', :foo, :bar)
+        ).to be false
+      end
+
+      it 'is aliased as BehavesAs?' do
+        Functional::BehaviorInfo(:foo) do
+          method(:bar, 0)
+          class_method(:baz, 0)
+        end
+
+        clazz = Class.new do
+          def bar(); nil; end
+          def self.baz(); nil; end
+        end
+
+        expect(Functional::BehaviorCheck::BehavesAs?(clazz.new, :foo)).to be true
+      end
     end
 
     context 'BehavesAs!' do
