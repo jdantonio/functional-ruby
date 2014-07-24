42130e5413324d0fc47177cd5755ec4b01cc923c
Jerry D'Antonio <stumpjumper@gmail.com>
2014-07-24 09:19:47 -0400

    Added underscores back to function names in PatternMatching module.

--- lib/functional/pattern_matching.rb
+++ lib/functional/pattern_matching.rb
@@ -42,8 +42,8 @@ module Functional
     end
 
     # @!visibility private
-    def self.match_pattern(args, pattern) # :nodoc:
-      return unless valid_pattern?(args, pattern)
+    def self.__match_pattern__(args, pattern) # :nodoc:
+      return unless __valid_pattern__?(args, pattern)
       pattern.each_with_index do |p, i|
         break if p == ALL && i+1 == pattern.length
         arg = args[i]
@@ -62,13 +62,13 @@ module Functional
     end
 
     # @!visibility private
-    def self.valid_pattern?(args, pattern) # :nodoc:
+    def self.__valid_pattern__?(args, pattern) # :nodoc:
       (pattern.last == ALL && args.length >= pattern.length) \
         || (args.length == pattern.length)
     end
 
     # @!visibility private
-    def self.unbound_args(match, args) # :nodoc:
+    def self.__unbound_args__(match, args) # :nodoc:
       argv = []
       match.first.each_with_index do |p, i|
         if p == ALL && i == match.first.length-1
@@ -85,18 +85,18 @@ module Functional
     end
 
     # @!visibility private
-    def self.pattern_match(clazz, func, *args, &block) # :nodoc:
+    def self.__pattern_match__(clazz, func, *args, &block) # :nodoc:
       args = args.first
 
-      matchers = clazz.function_pattern_matches[func]
+      matchers = clazz.__function_pattern_matches__[func]
       return Either.reason(:nodef) if matchers.nil?
 
       match = matchers.detect do |matcher|
-        if PatternMatching.match_pattern(args, matcher.first)
+        if PatternMatching.__match_pattern__(args, matcher.first)
           if matcher.last.nil?
             true # no guard clause
           else
-            self.instance_exec(*PatternMatching.unbound_args(matcher, args), &matcher.last)
+            self.instance_exec(*PatternMatching.__unbound_args__(matcher, args), &matcher.last)
           end
         end
       end
@@ -124,22 +124,22 @@ module Functional
           raise ArgumentError.new("block missing for definition of function `#{func}` on class #{self}")
         end
 
-        pattern = add_pattern_for(func, *args, &block)
+        pattern = __add_pattern_for__(func, *args, &block)
 
         unless self.instance_methods(false).include?(func)
-          define_method_with_matching(func)
+          __define_method_with_matching__(func)
         end
 
         return GUARD_CLAUSE.new(func, self, pattern)
       end
 
       # @!visibility private
-      def define_method_with_matching(func) # :nodoc:
+      def __define_method_with_matching__(func) # :nodoc:
         define_method(func) do |*args, &block|
-          match = PatternMatching.pattern_match(self.method(func).owner, func, args, block)
+          match = PatternMatching.__pattern_match__(self.method(func).owner, func, args, block)
           if match.value?
             # if a match is found call the block
-            argv = PatternMatching.unbound_args(match.value, args)
+            argv = PatternMatching.__unbound_args__(match.value, args)
             return self.instance_exec(*argv, &match.value[1])
           else # if result == :nodef || result == :nomatch
             begin
@@ -152,14 +152,14 @@ module Functional
       end
 
       # @!visibility private
-      def function_pattern_matches # :nodoc:
-        @function_pattern_matches ||= Hash.new
+      def __function_pattern_matches__ # :nodoc:
+        @__function_pattern_matches__ ||= Hash.new
       end
 
       # @!visibility private
-      def add_pattern_for(func, *args, &block) # :nodoc:
+      def __add_pattern_for__(func, *args, &block) # :nodoc:
         block = Proc.new{} unless block_given?
-        matchers = self.function_pattern_matches
+        matchers = self.__function_pattern_matches__
         matchers[func] = [] unless matchers.has_key?(func)
         matchers[func] << [args, block, nil]
         return matchers[func].last
