096e505ca903d05b6d5a4278987d1805d5f006d4
Jerry D'Antonio <stumpjumper@gmail.com>
2014-07-29 13:48:06 -0400

    More Memo documentation.

--- lib/functional/memo.rb
+++ lib/functional/memo.rb
@@ -2,6 +2,38 @@ require 'thread'
 
 module Functional
 
+  # @example
+  #   class Factors
+  #     include Functional::Memo
+  #   
+  #     def self.sum_of(number)
+  #       of(number).reduce(:+)
+  #     end
+  #   
+  #     def self.of(number)
+  #       (1..number).select {|i| factor?(number, i)}
+  #     end
+  #   
+  #     def self.factor?(number, potential)
+  #       number % potential == 0
+  #     end
+  #   
+  #     def self.perfect?(number)
+  #       sum_of(number) == 2 * number
+  #     end
+  #   
+  #     def self.abundant?(number)
+  #       sum_of(number) > 2 * number
+  #     end
+  #   
+  #     def self.deficient?(number)
+  #       sum_of(number) < 2 * number
+  #     end
+  #   
+  #     memoize(:sum_of)
+  #     memoize(:of)
+  #   end
+  #
   # @see http://en.wikipedia.org/wiki/Memoization Memoization (Wikipedia)
   # @see http://clojuredocs.org/clojure_core/clojure.core/memoize Clojure memoize
   module Memo
@@ -31,6 +63,18 @@ module Functional
       # @!visibility private
       attr_accessor :__method_memos__
 
+      # Returns a memoized version of a referentially transparent function. The
+      # memoized version of the function keeps a cache of the mapping from arguments
+      # to results and, when calls with the same arguments are repeated often, has
+      # higher performance at the expense of higher memory use.
+      #
+      # @param [Symbol] func the class/module function to memoize
+      # @param [Hash] opts the options controlling memoization
+      # @option opts [Fixnum] :at_most the maximum number of memos to store in the
+      #   cache; a value of zero (the default) or `nil` indicates no limit
+      #
+      # @raise [ArgumentError] when the method has already been memoized
+      # @raise [ArgumentError] when :at_most option is a negative number
       def memoize(func, opts = {})
         func = func.to_sym
         max_cache = opts[:at_most].to_i
@@ -38,6 +82,7 @@ module Functional
         raise ArgumentError.new(':max_cache must be > 0') if max_cache < 0
         __method_memos__[func] = Memo.new(method(func), Mutex.new, {}, max_cache.to_i)
         __define_memo_proxy__(func)
+        nil
       end
 
       # @!visibility private
