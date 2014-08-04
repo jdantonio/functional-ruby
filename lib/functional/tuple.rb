f3a7bca5545d1e56e75226cc8a65b6cfe8f7f46a
Jerry D'Antonio <stumpjumper@gmail.com>
2014-08-03 22:39:39 -0400

    wip: Created empty Tuple class.

---
+++ lib/functional/tuple.rb
@@ -0,0 +1,13 @@
+module Functional
+
+  # @see http://en.wikipedia.org/wiki/Tuple
+  # @see http://msdn.microsoft.com/en-us/library/system.tuple.aspx
+  # @see http://www.tutorialspoint.com/python/python_tuples.htm
+  # @see http://en.cppreference.com/w/cpp/utility/tuple
+  # @see http://docs.oracle.com/javaee/6/api/javax/persistence/Tuple.html
+  # @see http://www.erlang.org/doc/reference_manual/data_types.html
+  # @see http://www.erlang.org/doc/man/erlang.html#make_tuple-2
+  class Tuple
+
+  end
+end
---
+++ spec/functional/tuple_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+module Functional
+
+  describe Tuple do
+
+    pending
+
+  end
+end
